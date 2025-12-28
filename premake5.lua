1473dc2c924c9c47dfb1b6e21c75d01323c8cf96
Mathurin Lemarie <mathurin.lemarie@kitware.com>
2025-12-28 12:43:45 +0100

    Add linux support

--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,7 @@ project "GLFW"
     {
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
+
 		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
@@ -23,12 +24,14 @@ project "GLFW"
         "src/null_init.c",
         "src/null_monitor.c",
         "src/null_window.c",
-        "src/null_joystick.c"
+        "src/null_joystick.c",
+
+        "src/egl_context.c",
+        "src/osmesa_context.c"
     }
 
     filter "system:windows"
         systemversion "latest"
-    
         files
         {
             "src/win32_init.c",
@@ -38,15 +41,43 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+            "src/wgl_context.c",
         }
 
         defines
         {
             "_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
+        }
+
+    filter "system:linux"
+        files
+        {
+            "src/linux_joystick.c",
+            "src/linux_joystick.h",
+
+            "src/x11_platform.h",
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+
+            "src/xkb_unicode.h",
+            "src/xkb_unicode.c",
+
+            "src/glx_context.c",
+
+            "src/posix_module.c",
+            "src/posix_poll.h",
+            "src/posix_poll.c",
+            "src/posix_thread.h",
+            "src/posix_thread.c",
+            "src/posix_time.h",
+            "src/posix_time.c"
+        }
+
+        defines
+        {
+            "_GLFW_X11"
         }
 
     filter "configurations:Debug"
